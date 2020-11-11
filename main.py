3bd1c48b2a70c5fca941acfdcf2d723385570358
Tareq Judeh <tareq.joudeh@gmail.com>
2020-11-11 13:24:09 +0000

    Uploading project file to GitHub.

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 #Step 1 
+from random import choice
 
 word_list = ["aardvark", "baboon", "camel"]
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-
+chosen_word = choice(word_list)
+print(chosen_word)
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+guess = input("Guess a letter: ").lower()
 
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+check_answer = guess in chosen_word
+if check_answer:
+  print("Right")
+else:
+  print("Wrong")
+
+ 
